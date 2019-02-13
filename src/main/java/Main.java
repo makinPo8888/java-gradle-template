362387ba639b282d8fa1665ea0c9f7f9266641e0
makinPo8888 <kmaki00123@gmail.com>
2019-02-14 01:36:33 +0900

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        HelloWorld helloWorld = new HelloWorld();
+
+        System.out.println(helloWorld.sayHello());
+    }
+}
